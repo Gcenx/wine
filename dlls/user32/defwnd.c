3f7c55837facaa1e3ab5c3d68dc356449fb3c5a5
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-08-20 19:49:29 -0400

    Revert "user32: Set WM_CONTEXTMENU's wparam to the child window's handle."
    This reverts commit 149284628def87f7e1ca70a1f8c7858d68cea346.

--- dlls/user32/defwnd.c
+++ dlls/user32/defwnd.c
@@ -311,7 +311,7 @@ static LRESULT DEFWND_DefWinProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPa
 
     case WM_CONTEXTMENU:
         if (GetWindowLongW( hwnd, GWL_STYLE ) & WS_CHILD)
-            SendMessageW( GetParent(hwnd), msg, (WPARAM)hwnd, lParam );
+            SendMessageW( GetParent(hwnd), msg, wParam, lParam );
         else
         {
             LONG hitcode;
