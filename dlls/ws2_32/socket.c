Gcenx/wine

commit bf24c316f443185c16513118e30e160b20223134

ws2_32: Handle more IGMP socket options.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47956
Signed-off-by: Fabian Maurer <dark.shadow4@web.de>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/ws2_32/socket.c
@@ -692,6 +692,10 @@ static const int ws_ip_map[][2] =
     MAP_OPTION( IP_MULTICAST_LOOP ),
     MAP_OPTION( IP_ADD_MEMBERSHIP ),
     MAP_OPTION( IP_DROP_MEMBERSHIP ),
+    MAP_OPTION( IP_ADD_SOURCE_MEMBERSHIP ),
+    MAP_OPTION( IP_DROP_SOURCE_MEMBERSHIP ),
+    MAP_OPTION( IP_BLOCK_SOURCE ),
+    MAP_OPTION( IP_UNBLOCK_SOURCE ),
     MAP_OPTION( IP_OPTIONS ),
 #ifdef IP_HDRINCL
     MAP_OPTION( IP_HDRINCL ),
@@ -5801,6 +5805,7 @@ int WINAPI WS_setsockopt(SOCKET s, int level, int optname,
     int woptval;
     struct linger linger;
     struct timeval tval;
+    struct ip_mreq_source mreq_source;
 
     TRACE("(socket %04lx, %s, optval %s, optlen %d)\n", s,
           debugstr_sockopt(level, optname), debugstr_optval(optval, optlen),
@@ -6011,6 +6016,22 @@ int WINAPI WS_setsockopt(SOCKET s, int level, int optname,
     case WS_IPPROTO_IP:
         switch(optname)
         {
+        case WS_IP_ADD_SOURCE_MEMBERSHIP:
+        case WS_IP_DROP_SOURCE_MEMBERSHIP:
+        case WS_IP_BLOCK_SOURCE:
+        case WS_IP_UNBLOCK_SOURCE:
+        {
+            WS_IP_MREQ_SOURCE* val = (void*)optval;
+            mreq_source.imr_interface.s_addr = val->imr_interface.S_un.S_addr;
+            mreq_source.imr_multiaddr.s_addr = val->imr_multiaddr.S_un.S_addr;
+            mreq_source.imr_sourceaddr.s_addr = val->imr_sourceaddr.S_un.S_addr;
+
+            optval = (char*)&mreq_source;
+            optlen = sizeof(mreq_source);
+
+            convert_sockopt(&level, &optname);
+            break;
+        }
         case WS_IP_ADD_MEMBERSHIP:
         case WS_IP_DROP_MEMBERSHIP:
 #ifdef IP_HDRINCL

commit 4e3860b567c9c25c651c4e5625a97a77155f8c70

ws2_32: guard IGMP socket options
Resolves issues caused by https://gitlab.winehq.org/wine/wine/-/commit/bf24c316f443185c16513118e30e160b20223134

dlls/ws2_32/socket.c
@@ -658,10 +658,18 @@ static const int ws_ip_map[][2] =
     MAP_OPTION( IP_MULTICAST_LOOP ),
     MAP_OPTION( IP_ADD_MEMBERSHIP ),
     MAP_OPTION( IP_DROP_MEMBERSHIP ),
+#ifdef IP_ADD_SOURCE_MEMBERSHIP
     MAP_OPTION( IP_ADD_SOURCE_MEMBERSHIP ),
+#endif
+#ifdef IP_DROP_SOURCE_MEMBERSHIP
     MAP_OPTION( IP_DROP_SOURCE_MEMBERSHIP ),
+#endif
+#ifdef IP_BLOCK_SOURCE
     MAP_OPTION( IP_BLOCK_SOURCE ),
+#endif
+#ifdef IP_UNBLOCK_SOURCE
     MAP_OPTION( IP_UNBLOCK_SOURCE ),
+#endif
     MAP_OPTION( IP_OPTIONS ),
 #ifdef IP_HDRINCL
     MAP_OPTION( IP_HDRINCL ),
@@ -5215,7 +5223,9 @@ int WINAPI WS_setsockopt(SOCKET s, int level, int optname,
     int woptval;
     struct linger linger;
     struct timeval tval;
+#if MAC_OS_X_VERSION_MAX_ALLOWED > 1070
     struct ip_mreq_source mreq_source;
+#endif
 
     TRACE("(socket %04lx, %s, optval %s, optlen %d)\n", s,
           debugstr_sockopt(level, optname), debugstr_optval(optval, optlen),
@@ -5431,6 +5441,7 @@ int WINAPI WS_setsockopt(SOCKET s, int level, int optname,
         case WS_IP_BLOCK_SOURCE:
         case WS_IP_UNBLOCK_SOURCE:
         {
+#if MAC_OS_X_VERSION_MAX_ALLOWED > 1070
             WS_IP_MREQ_SOURCE* val = (void*)optval;
             mreq_source.imr_interface.s_addr = val->imr_interface.S_un.S_addr;
             mreq_source.imr_multiaddr.s_addr = val->imr_multiaddr.S_un.S_addr;
@@ -5440,6 +5451,7 @@ int WINAPI WS_setsockopt(SOCKET s, int level, int optname,
             optlen = sizeof(mreq_source);
 
             convert_sockopt(&level, &optname);
+#endif
             break;
         }
         case WS_IP_ADD_MEMBERSHIP: